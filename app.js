cad615a199f2f7c2d4444f2b078ec454a673a36d
Elvis Knapman <elvis@elvisknapman.com>
2019-01-26 12:55:15 -0500

    Add code for CSS transition classes

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; // stop function from running if no associated key code found
     
     // set current audio time to 0 so it doesn't wait until end of audio file to be able to be triggered again by key press
     audio.currentTime = 0;
     audio.play();
+    key.classList.add('playing');
 });
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; // skip if it's not a transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
