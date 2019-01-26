cd6353e12beeaf5894f2ea2111d41cb5faf6929d
Elvis Knapman <elvis@elvisknapman.com>
2019-01-26 12:36:42 -0500

    Change currentTime of audio file to 0 on each key press to prevent audio from not playing  on multiple key presses

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if(!audio) return; // stop function from running if no associated key code found
+    
+    // set current audio time to 0 so it doesn't wait until end of audio file to be able to be triggered again by key press
+    audio.currentTime = 0;
     audio.play();
 });
