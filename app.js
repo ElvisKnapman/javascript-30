ad5f8149def6a73ea63de634bbecdd1c4357deed
Elvis Knapman <elvis@elvisknapman.com>
2019-01-26 13:00:57 -0500

    refactored anonymous function that played sounds etc. into separate function declaration

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown', function(e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; // stop function from running if no associated key code found
@@ -7,7 +7,7 @@ window.addEventListener('keydown', function(e) {
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-});
+}
 
 function removeTransition(e) {
     if(e.propertyName !== 'transform') return; // skip if it's not a transform
@@ -16,3 +16,5 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
